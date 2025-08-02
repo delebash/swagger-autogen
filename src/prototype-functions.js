3898f8e8fe5a735b4d589db414a995c692698a2e
delebash <danelebash@hotmail.com>
2025-08-01 20:35:08 -0400

    merge pull requests from origin

--- src/prototype-functions.js
+++ src/prototype-functions.js
@@ -1,5 +1,3 @@
-if (String.prototype.replaceAll == null) {
-    String.prototype.replaceAll = function (search, replacement) {
-        return this.split(search).join(replacement);
-    };
-}
+String.prototype.replaceAll = function (search, replacement) {
+    return this.split(search).join(replacement);
+};
